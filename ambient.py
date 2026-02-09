cad7102deaa5bbb1ac10c8f3c535b3c66ab35c26
czjstmax <jstmaxlol@disroot.org>
2026-02-09 17:44:57 +0100

    Edited command prefix. From `!` to `;;`

--- ambient.py
+++ ambient.py
@@ -11,7 +11,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix=";;", intents=intents)
 
 ytdl_playlist_opts = {
     "quiet": True,
