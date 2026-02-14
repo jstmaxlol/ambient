4a9f63d549ef6fa2a5d4f5920b74dcbed6f86e3a
czjstmax <jstmaxlol@disroot.org>
2026-02-14 21:04:55 +0100

    better `::help`
    Signed-off-by: czjstmax <jstmaxlol@disroot.org>

--- ambient.py
+++ ambient.py
@@ -8,13 +8,13 @@ import random
 TOKEN = ""
 PLAYLIST_URL = "https://www.youtube.com/playlist?list=PLuI-iSzcTZFUfVlc9OoZL5LbubQ_T_st9"
 
-volume = 0.6
+volume = 0.33
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
 
-bot = commands.Bot(command_prefix=":", intents=intents)
+bot = commands.Bot(command_prefix="::", intents=intents, help_command=None)
 
 ytdl_playlist_opts = {
     "quiet": True,
@@ -63,7 +63,7 @@ async def play_loop(vc: discord.VoiceClient):
 async def on_ready():
     print(f"[ambient/status] logged in as {bot.user}")
 
-@bot.command()
+@bot.command(aliases=['join','hi','gm'])
 async def start(ctx):
     global play_task
 
@@ -95,14 +95,12 @@ async def start(ctx):
 
     await ctx.send("♥ joined vc, deafened, looping playlist~! 🌫️🎶")
 
-@bot.command()
+@bot.command(aliases=['quit','bye','gn'])
 async def stop(ctx):
     vc = discord.utils.get(bot.voice_clients, guild=ctx.guild)
     if vc and vc.is_connected():
         await vc.disconnect()
         await ctx.send("♥ disconnected.. bye bye~~!")
-
-bot.run(TOKEN)
 
 @bot.command(aliases=['vol'])
 async def volume_cmd(ctx, vol: str):
@@ -115,7 +113,7 @@ async def volume_cmd(ctx, vol: str):
         if n < 0 or n > 100:
             raise ValueError
     except ValueError:
-        await ctx.send("♥ volume must be 0-100%")
+        await ctx.send("♥ volume must be 0-100% ~~")
         return
 
     volume = n / 100
@@ -125,15 +123,17 @@ async def volume_cmd(ctx, vol: str):
 
     await ctx.send(f"♥ volume set to {n}% 🌫️")
 
-@bot.command()
+@bot.command(aliases=['?', 'h'])
 async def help(ctx):
     await ctx.send(
-        "ambient.py --- with ♥ by czjstmax\n"
+        "\nambient.py --- with ♥ by czjstmax\n"
         "> server: discord.gg/6mbgsYveCn\n"
         "> github: github.com/jstmaxlol/ambient\n\n"
-        ":: available commands ::\n"
-        ":start - join a vc and use this command to eep ;)\n"
-        ":stop - exits from the current vc and salutes\n"
-        ":help - shows this message~!"
+        "### >> available commands <<\n"
+        "`::start` / `::join` / `::hi` / `::gm` - join a vc and use this command to eep ;)\n"
+        "`::stop` / `::quit` / `::bye` / `::gn` - exits from the current vc and salutes\n"
+        "`::help` / `::?` / `::h` - shows this help/usage message~!"
     )
+
+bot.run(TOKEN)
 
