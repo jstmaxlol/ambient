af7925bec7ef147f64b08f561a04325cb2bd7a0a
czjstmax <jstmaxlol@disroot.org>
2026-02-15 16:40:11 +0100

    fixed `:vol ?` --- more in commit desc.
    `:vol ?` now returns value of `volume` rounded up and in percent
    instead of stupid ahh guide
    
    Signed-off-by: czjstmax <jstmaxlol@disroot.org>

--- ambient.py
+++ ambient.py
@@ -107,7 +107,8 @@ async def volume_cmd(ctx, vol: str):
     """set volume in % from 0-100"""
     global volume
     if vol == "?":
-        await ctx.send(f"♥ to set the volume just type `:vol 50%` ~~!");
+        #await ctx.send(f"♥ to set the volume just type `:vol 50%` ~~!")
+        await ctx.send(f"♥ the current volume is {round(volume*100)}% ~~!")
         return 1
 
     try:
